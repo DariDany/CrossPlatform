b83f59c8846b87c47434f7e80fbeb9c354303d44
DariDany <daridanyi70405@gmail.com>
2024-11-09 10:23:30 +0200

    Lab4

---
+++ Labs/LabsLibrary/EntCounter.cs
@@ -0,0 +1,77 @@
+﻿
+namespace LabsLibrary
+{
+    public class EntCounter
+    {
+        private int K;
+        private int P;
+
+        public EntCounter(int k, int p)
+        {
+            // Перевірка на відповідність умовам
+            if (k <= 0 || k > 1000000)
+            {
+                throw new ArgumentOutOfRangeException(nameof(k), "K повинно бути в межах 1 ≤ K ≤ 10^6.");
+            }
+
+            if (p <= 0 || p > 1000000000)
+            {
+                throw new ArgumentOutOfRangeException(nameof(p), "P повинно бути в межах 1 ≤ P ≤ 10^9.");
+            }
+
+            K = k;
+            P = p;
+        }
+
+        public long CountEnts()
+        {
+            long[] dp = new long[K + 1];
+
+            if (K >= 2) dp[2] = 1;
+
+            for (int i = 2; i <= K; i++)
+            {
+                if (dp[i] > 0)
+                {
+                    if (i + 1 <= K)
+                    {
+                        dp[i + 1] = (dp[i + 1] + dp[i]) % P;
+                    }
+
+                    if (i * 2 <= K)
+                    {
+                        dp[i * 2] = (dp[i * 2] + dp[i]) % P;
+                    }
+                }
+            }
+
+            return dp[K] % P;
+        }
+
+        public void WriteOutput(long result, string output)
+        {
+            File.WriteAllText(output, result.ToString());
+        }
+
+        public static EntCounter ReadInput(string fileName)
+        {
+            string[] input = File.ReadAllText(fileName).Split();
+            if (input.Length != 2)
+            {
+                throw new FormatException("Вхідний файл повинен містити два натуральних числа.");
+            }
+
+            if (!int.TryParse(input[0], out int k))
+            {
+                throw new FormatException("Невірний формат числа K.");
+            }
+
+            if (!int.TryParse(input[1], out int p))
+            {
+                throw new FormatException("Невірний формат числа P.");
+            }
+
+            return new EntCounter(k, p);
+        }
+    }
+}
---
+++ Labs/LabsLibrary/LabRunner.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabsLibrary
+{
+    public class LabRunner
+    {
+        public void RunLab1(string inputFile, string outputFile)
+        {
+            // Create an instance of the FindLength class
+            FindLength findLength = new FindLength();
+
+            // Call the Find method to execute the functionality
+            findLength.Find(inputFile, outputFile);
+
+        }
+        public void RunLab2(string inputFile, string outputFile)
+        {
+            try
+            {
+                EntCounter counter = EntCounter.ReadInput(inputFile);
+                long result = counter.CountEnts();
+                counter.WriteOutput(result, outputFile);
+            }
+            catch (Exception ex)
+            {
+                // Лог помилки або виведення повідомлення
+                Console.WriteLine("Помилка: " + ex.Message);
+            }
+        }
+        public void RunLab3(string inputFile, string outputFile)
+        {
+            try
+            {
+                // Створення екземпляра класу RectangleAreaSolver
+                RectangleAreaSolver solver = new RectangleAreaSolver();
+
+                // Зчитування вхідних даних
+                solver.ReadInput(inputFile);
+
+                // Обчислення найбільшої площі фігури
+                int maxArea = solver.FindMaxArea();
+
+                // Виведення результату
+                solver.WriteOutput(outputFile, maxArea);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Помилка: " + ex.Message);
+            }
+        }
+    }
+}
