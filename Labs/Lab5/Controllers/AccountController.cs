66758890d1fec8d4213fdfebfa74e4b710d61e54
DariDany <daridanyi70405@gmail.com>
2024-11-09 19:47:50 +0200

    lab5

---
+++ Labs/Lab5/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab5.Controllers
+{
+    public class AccountController : Controller
+    {
+        public async Task Login(string returnUrl = "/")
+        {
+            var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+                .WithRedirectUri(returnUrl)
+                .Build();
+
+            await HttpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+        }
+
+        [Authorize]
+        public async Task Logout()
+        {
+            var authenticationProperties = new LogoutAuthenticationPropertiesBuilder()
+                .WithRedirectUri(Url.Action("Index", "Home"))
+                .Build();
+
+            // Очистить аутентификацию через Auth0
+            await HttpContext.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+
+            // Очистить стандартные cookies аутентификации
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            // Очистить все cookies, чтобы предотвратить автоматический вход
+            Response.Cookies.Delete(".AspNetCore.Cookies");
+        }
+
+        public async Task Register(string returnUrl = "/")
+        {
+            var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+                .WithRedirectUri(returnUrl)
+                .Build();
+
+            await HttpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+        }
+    }
+}
---
+++ Labs/Lab5/Controllers/LabsController.cs
@@ -0,0 +1,97 @@
+﻿using System.Diagnostics;
+using Lab5.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using LabsLibrary;
+
+namespace Lab5.Controllers
+{
+    public class LabsController : Controller
+    {
+        //[Authorize]
+        [HttpGet]
+        public IActionResult FindLength()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult FindLength(IFormFile inputFile)
+        {
+            if (inputFile == null || inputFile.Length == 0)
+            {
+                ViewBag.Result = "Будь ласка, завантажте файл.";
+                return View();
+            }
+            string inputFilePath = Path.Combine(Path.GetTempPath(), inputFile.FileName);
+            using (var stream = new FileStream(inputFilePath, FileMode.Create))
+            {
+                inputFile.CopyTo(stream);
+            }
+            string outputFilePath = Path.Combine(Path.GetTempPath(), "output.txt");
+            LabRunner labRunner = new LabRunner();
+            labRunner.RunLab1(inputFilePath, outputFilePath);
+            string result = System.IO.File.ReadAllText(outputFilePath);
+            ViewBag.Result = result;
+
+            return View();
+        }
+
+        //[Authorize]
+        [HttpGet]
+        public IActionResult EntCounter()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult EntCounter(IFormFile inputFile)
+        {
+            if (inputFile == null || inputFile.Length == 0)
+            {
+                ViewBag.Result = "Будь ласка, завантажте файл.";
+                return View();
+            }
+            string inputFilePath = Path.Combine(Path.GetTempPath(), inputFile.FileName);
+            using (var stream = new FileStream(inputFilePath, FileMode.Create))
+            {
+                inputFile.CopyTo(stream);
+            }
+            string outputFilePath = Path.Combine(Path.GetTempPath(), "output.txt");
+            LabRunner labRunner = new LabRunner();
+            labRunner.RunLab2(inputFilePath, outputFilePath);
+            string result = System.IO.File.ReadAllText(outputFilePath);
+            ViewBag.Result = result;
+
+            return View();
+        }
+
+        //[Authorize]
+        [HttpGet]
+        public IActionResult RectangleAreaSolver()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RectangleAreaSolver(IFormFile inputFile)
+        {
+            if (inputFile == null || inputFile.Length == 0)
+            {
+                ViewBag.Result = "Будь ласка, завантажте файл.";
+                return View();
+            }
+            string inputFilePath = Path.Combine(Path.GetTempPath(), inputFile.FileName);
+            using (var stream = new FileStream(inputFilePath, FileMode.Create))
+            {
+                inputFile.CopyTo(stream);
+            }
+            string outputFilePath = Path.Combine(Path.GetTempPath(), "output.txt");
+            LabRunner labRunner = new LabRunner();
+            labRunner.RunLab3(inputFilePath, outputFilePath);
+            string result = System.IO.File.ReadAllText(outputFilePath);
+            ViewBag.Result = result;
+
+            return View();
+        }
+    }
+}
